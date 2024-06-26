41d92555086763cefdcc2e1f202077e680b199a3
saishibunb <saishibu@jupitermeta.io>
2024-06-27 01:14:20 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -17,12 +17,18 @@ copy_if_not_exists() {
   fi
 }
 
+echo pwd
 pwd
 
+echo ls
+ls
+
+cd app
+echo app ls
 ls
 
 # Copy application files to persistent storage if they don't exist
 copy_if_not_exists /app /mnt/app
 
 # Run the main script
-exec /mnt/app/run.sh
+exec /app/run.sh
