925b75dce255a3f379588e80b0daaf5ae84b32c5
saishibunb <144234378+saishibunb@users.noreply.github.com>
2024-07-02 13:09:46 +0530

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-logfilerubix="/home/saishibu/cronlog/rubixstartup.log"
-logfileBridge="/home/saishibu/cronlog/bridgestartup.log"
+logfilerubix="/cronlog/rubixstartup.log"
+logfileBridge="/cronlog/bridgestartup.log"
 
 #screen -dmS node2 -L -Logfile $log_file $runfile
 echo "Starting Rubix" >> $logfilerubix
-screen -dmS rubix /home/saishibu/Rubix-Docker/rubix/rubixgoplatform run -p node1 -n 0 -s -port 20000 -testNet -grpcPort 10500
+screen -dmS rubix /Rubix-Docker/rubix/rubixgoplatform run -p node1 -n 0 -s -port 20000 -testNet -grpcPort 10500
 echo "Started Rubix" >> $logfilerubix
 
 echo "Starting Bridge" >> $logfileBridge
-screen -dmS bridge /home/saishibu/Rubix-Docker/middleware/bridge.py
+screen -dmS bridge /Rubix-Docker/middleware/bridge.py
 echo "Started Bridge" >> $logfileBridge
