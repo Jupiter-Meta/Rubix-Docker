d3fe64bc16a599b110eea244924f007cc2d1115f
saishibunb <144234378+saishibunb@users.noreply.github.com>
2024-09-05 14:45:50 +0530

    Update dbConnect.py

--- middleware/dbConnect.py
+++ middleware/dbConnect.py
@@ -137,6 +137,7 @@ def fetchUserTransactionswithID(userID):
 
 def fetchUserAttributeswithID(userID):
     userid_cleaned = userID.replace("'", "")
+    
     connection_params = {
     'host': 'udchalo-preprod-db.cluster-cnhilnddjsql.ap-south-1.rds.amazonaws.com',
     'port': 5432,
@@ -179,4 +180,40 @@ def fetchUserAttributeswithID(userID):
             cur.close()
         if conn:
             conn.close()
+
+def writeAttributes(data):
+    connection_params = {
+    'host': 'udchalo-preprod-db.cluster-cnhilnddjsql.ap-south-1.rds.amazonaws.com',
+    'port': 5432,
+    'database': 'rubix_dev',
+    'user': 'superj_rubix',      # Replace with your actual username
+    'password': 'superj_rubix'   # Replace with your actual password
+    }
+    conn = None
+    cur = None
+    try:
+        # Connect to the PostgreSQL database
+        conn = psycopg2.connect(**connection_params)
+        
+        # Create a cursor object
+        cur = conn.cursor()
+        insert_query = """INSERT INTO userattributes (did, userid, onboarded, keywords, attributes) VALUES (%s, %s, %s, %s, %s);"""
+        # Execute a query
+        cursor.execute(insert_query, (data["did"], data["userId"], data["onboarded"], data["keywords"], data["attributes"]))
+        
+        # Fetch all rows from the executed query
+        connection.commit()
+        
+        return 1
+        
+    except Exception as e:
+        print("An error occurred:", e)
+        return None
+    finally:
+        # Close the cursor and connection
+        if cur:
+            cur.close()
+        if conn:
+            conn.close()
+    
     
